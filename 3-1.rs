388c4fc5beb6a344f5608f0f565812668f85d378
Lakkoju-Sravya <22pa1a0587@vishnu.edu.in>
2025-07-02 22:28:25 +0530

    Create 3-1.rs

---
+++ 3-1.rs
@@ -0,0 +1,12 @@
+fn main() {
+  let number = 5;
+  if number > 0 {
+      println!("The number is positive");
+  }
+  else if number < 0 {
+      println!("The number is negitive");
+  }
+  else{
+      println!("The number is zero");
+  }
+}
